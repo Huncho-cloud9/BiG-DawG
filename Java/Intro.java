7159ca9d8ecb030b3fd24cb6af2eb95cfedfeea6
HUNCHO <dumbledorehuncho@gmail.com>
2025-08-19 15:20:23 +0100

    repository

---
+++ Java/Intro.java
@@ -0,0 +1,1 @@
+byte whole = 129; 
---
+++ Java/Rect.java
@@ -0,0 +1,46 @@
+ short whole = 128;
+double deci = 10.123456789111213d;
+String bigA = "A";
+String smallA = "a";
+boolean anything = (bigA == smallA);
+system.out.printIn();
+
+int noOfcheeseballs = 50;
+float pricePerItem = 9.99f;
+char currency = '$';
+float totalPrice = noOfcheeseballs * pricePerItem;
+System.out.printIn("for the cheese balls" + totalPrice)
+
+int noOffruitballs = 12;
+float priceofFB = 10.99f;
+float totalpriceFB = noOffruitballs * priceofFB;
+System.out.printIn("for the fruit balls" + totalPrice)
+
+float totalsum = totalPriceCB + totalpriceFB;
+System.out.printIn("for the fruit balls" + totalPrice)
+
+public class AgeCal{
+    public static void main(String[]args){
+    //CALCULATE AGE BASED ON BIRTH YEAR
+    // RESULT THEIR ACTUAL AGE
+
+    Scanner scanner = new Scanner(System.in);
+    int birthYear;
+    System.out.print("Enter your birthYear : ");
+    birthYear = scanner.nextInt();
+    int result = 2025 - birthYear;
+    System.out.printIn("You are currently" + result + "years old");
+    }
+}
+
+import 
+
+
+
+
+
+
+
+
+
+
